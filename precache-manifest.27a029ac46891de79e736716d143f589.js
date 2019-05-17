7932e18baef66a82afe348eed829511be9b172d1
Chelsia Liu <chelsia.liu3@gmail.com>
2019-05-17 12:04:45 -0600

    Updates

---
+++ precache-manifest.27a029ac46891de79e736716d143f589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d8db3aaad5d4f2ce104f3c2fea2c07",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "678bc7d1606ad9c4e83b",
+    "url": "/seasons/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/seasons/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "678bc7d1606ad9c4e83b",
+    "url": "/seasons/static/js/main.afb3dc64.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/seasons/static/media/logo.5d5d9eef.svg"
+  }
+]);
